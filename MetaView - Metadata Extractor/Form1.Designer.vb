161a0059ef93de4ef96c87ef446549bb1f76ac23
Mohammadsaad Mulla <83002941+codelust@users.noreply.github.com>
2021-09-22 14:23:14 +0530

    Add files via upload

---
+++ MetaView - Metadata Extractor/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PathBox = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PathBox
+        '
+        Me.PathBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PathBox.Location = New System.Drawing.Point(14, 12)
+        Me.PathBox.Name = "PathBox"
+        Me.PathBox.Size = New System.Drawing.Size(267, 22)
+        Me.PathBox.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(292, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(373, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Extract"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.Color.White
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.RichTextBox1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(14, 51)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(434, 298)
+        Me.RichTextBox1.TabIndex = 5
+        Me.RichTextBox1.TabStop = False
+        Me.RichTextBox1.Text = ""
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopyToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(103, 26)
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(102, 22)
+        Me.CopyToolStripMenuItem.Text = "Copy"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.Image = Global.MetaView___Metadata_Extractor.My.Resources.Resources.gmaps
+        Me.PictureBox2.Location = New System.Drawing.Point(465, 319)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(30, 30)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Image = Global.MetaView___Metadata_Extractor.My.Resources.Resources.image_icon
+        Me.PictureBox1.Location = New System.Drawing.Point(465, 11)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(337, 337)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(814, 361)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PathBox)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(830, 400)
+        Me.MinimumSize = New System.Drawing.Size(830, 400)
+        Me.Name = "Form1"
+        Me.Text = "MetaView - Metadata Extractor"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PathBox As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ MetaView - Metadata Extractor/Form1.vb
@@ -0,0 +1,142 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Dim path As String
+    Dim tool As String
+    Dim Target() As String
+    Dim url As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox2.Visible = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (OpenFileDialog1.ShowDialog() = DialogResult.OK) Then
+            PathBox.Text = OpenFileDialog1.SafeFileName
+            path = OpenFileDialog1.FileName
+            tool = My.Computer.FileSystem.SpecialDirectories.Temp + "exiftool.exe"
+            IO.File.WriteAllBytes(tool, My.Resources.exiftool)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        PictureBox2.Visible = False
+
+        If path = "" Then
+            MessageBox.Show("File Not Found")
+        Else
+            RichTextBox1.SelectionStart = 0
+            RichTextBox1.SelectionLength = 0
+
+            If path.Contains(".jpg") = True Or path.Contains(".png") = True Then
+                PictureBox1.ImageLocation = path
+            ElseIf path.Contains(".mp4") = True Or path.Contains(".avi") = True Or path.Contains(".mkv") = True Then
+                PictureBox1.Image = My.Resources.video_icon
+            Else
+                PictureBox1.Image = My.Resources.file_icon
+            End If
+
+            RichTextBox1.Text = ""
+
+            Dim p = New System.Diagnostics.Process()
+            p.StartInfo.FileName = tool
+            p.StartInfo.Arguments = """" + path + """"
+            p.StartInfo.UseShellExecute = False
+            p.StartInfo.CreateNoWindow = True
+            p.StartInfo.RedirectStandardOutput = True
+            p.Start()
+
+            RichTextBox1.Text = p.StandardOutput.ReadToEnd()
+
+            If RichTextBox1.Text.Contains("GPS Position") Then
+                GPS()
+            End If
+
+        End If
+    End Sub
+
+    Sub GPS()
+
+        PictureBox2.Visible = True
+
+        Dim T1 As String = "GPS Position                    : "
+
+        Dim pFrom As Integer = RichTextBox1.Text.IndexOf(T1) + T1.Length
+        Dim position As String = RichTextBox1.Text.Substring(pFrom, 40)
+        'MessageBox.Show(position)
+        position = position.Replace(" deg ", " ")
+        position = position.Replace(", ", " ")
+        position = position.Replace("' ", " ")
+        position = position.Replace(""" ", " ")
+
+        'MessageBox.Show(position)
+
+        Dim ch As Char
+        Dim count As Integer = 0
+        Dim d1, m1, s1, d2, m2, s2 As Double
+        Dim pole1, pole2 As Char
+        Dim latitude, longitude As Double
+
+        For Each ch In position
+            If Convert.ToInt32(ch) >= 48 And Convert.ToInt32(ch) <= 57 Or ch = "." Or ch = "N" Or ch = "E" Then
+                Select Case count
+                    Case 0
+                        Dim temp As String
+                        temp = temp + ch.ToString(ch)
+                        d1 = Convert.ToDouble(temp)
+                    Case 1
+                        Dim temp As String
+                        temp = temp + ch.ToString(ch)
+                        m1 = Convert.ToDouble(temp)
+                    Case 2
+                        Dim temp As String
+                        temp = temp + ch.ToString(ch)
+                        s1 = Convert.ToDouble(temp)
+                    Case 3
+                        pole1 = ch
+                    Case 4
+                        Dim temp As String
+                        temp = temp + ch.ToString(ch)
+                        d2 = Convert.ToDouble(temp)
+                    Case 5
+                        Dim temp As String
+                        temp = temp + ch.ToString(ch)
+                        m2 = Convert.ToDouble(temp)
+                    Case 6
+                        Dim temp As String
+                        temp = temp + ch.ToString(ch)
+                        s2 = Convert.ToDouble(temp)
+                    Case 7
+                        pole2 = ch
+                End Select
+            Else
+                count = count + 1
+            End If
+        Next
+
+        latitude = d1 + (m1 / 60) + (s1 / 3600)
+        longitude = d2 + (m2 / 60) + (s2 / 3600)
+
+        'MessageBox.Show(latitude)
+        'MessageBox.Show(longitude)
+
+        Dim glink As String = "www.google.com/maps/search/?api=1&query=" + latitude.ToString() + "%2C" + longitude.ToString()
+
+        RichTextBox1.Text = RichTextBox1.Text + "Google Maps Link       : " + glink
+
+        url = glink
+
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        RichTextBox1.Copy()
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Process.Start(url)
+    End Sub
+
+
+End Class
